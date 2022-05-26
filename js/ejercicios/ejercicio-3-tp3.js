7579d1eeac59c3298d5b0befb28ea256f11022d0
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-05-26 01:25:39 -0300

    Ejercicio 2 check

---
+++ js/ejercicios/ejercicio-3-tp3.js
@@ -0,0 +1,4 @@
+/* 3- Escribir un script que simule el lanzamiento de dos dados. Hacer uso de 
+la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. 
+Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma,
+ repitiendo 50 veces esta operación.*/
