a3b67eac6fa2d8dcdb5feebd0c6134c82f4c6b4a
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-06-02 00:40:39 -0300

    Ejercicio tres

--- js/ejercicios/ejercicio-3-tp3.js
+++ js/ejercicios/ejercicio-3-tp3.js
@@ -2,3 +2,17 @@
 la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. 
 Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma,
  repitiendo 50 veces esta operación.*/
+
+ let dadoUno;
+ let dadoDos;
+ 
+alert('Lanza el dado numero uno');
+    dadoUno = parseInt(Math.random()* (6 - 1) + 1);
+alert('Lanza el dado numero dos');
+    dadoDos = parseInt(Math.random()* (6 - 1) + 1);
+ 
+
+document.write(`El valor del dado uno es ${dadoUno} <br>`);
+document.write(`El valor del dado dos es ${dadoDos} <br>`);
+
+let suma = dadoUno + dadoDos;
