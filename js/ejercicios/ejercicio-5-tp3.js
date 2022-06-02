556275de05489695eb9a32bb57ebe15395364b61
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-06-02 00:42:35 -0300

    Ejercicio cinco

--- js/ejercicios/ejercicio-5-tp3.js
+++ js/ejercicios/ejercicio-5-tp3.js
@@ -1,3 +1,7 @@
+/*5- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento.
+ A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas,
+  sólo por minúsculas o por una mezcla de ambas.*/
 
+  let cadenaTexto = prompt('Ingrese cadena de texto');
 
 
